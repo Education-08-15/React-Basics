9b47fbdbf5a40bde0e3ae23f1275f140832d9cd9
Shikshya Gautam <nanu.gautam2016@gmail.com>
2021-08-15 19:41:29 -0400

    import classComponent

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ClassComponent from './component/ClassComponent';
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      Hello
+      <ClassComponent />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
