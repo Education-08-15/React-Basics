c2905849028c2536b424e622b9bb2ea5adfe54e4
Shikshya Gautam <nanu.gautam2016@gmail.com>
2021-08-15 17:45:20 -0400

    component imported

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import FunctionComponent from './components/FunctionalComponent';
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      Hello
+      <FunctionComponent name='Shikshya'>
+        <h1>Hello</h1>
+      </FunctionComponent>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
