4b6e27bd1730431c04066de5bacd9ab6a7cea102
Shikshya Gautam <nanu.gautam2016@gmail.com>
2021-08-15 21:43:54 -0400

    components imported in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import WithJSX from './components/withJSX';
+import WithoutJSX from './components/withoutJSX';
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <WithJSX />
+      <WithoutJSX />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
