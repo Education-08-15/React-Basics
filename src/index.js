22f4d7044673b9e1655c53882c6b7e19c93966b6
Shikshya Gautam <nanu.gautam2016@gmail.com>
2021-08-16 12:27:56 -0400

    index.js updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Counter from './components/Counter';
+import Message from './components/Message';
 
 const App = () => {
   return <div>Hello</div>;
