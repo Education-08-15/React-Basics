be5c3af6d6e7913f5b1724f02b9c8f5d5cfeb29c
Shikshya Gautam <nanu.gautam2016@gmail.com>
2021-08-15 22:09:11 -0400

    components imported

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Props from './components/Props';
+import Props1 from './components/props1';
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <h1>Props in Functional Component</h1>
+      <Props name='Shikshya' profession='Coder'>
+        This is a children prop.
+        <button>click</button>
+      </Props>
+      <Props name='Nanu' profession='Programmer' />
+
+      <h1>Props in Class Component</h1>
+      <Props1 name='Shikshya' profession='Coder' />
+      <Props1 name='Nanu' profession='Programmer' />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
