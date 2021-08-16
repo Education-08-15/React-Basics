15ad4b309ad03c136031e0ae087331c0ce358770
Shikshya Gautam <nanu.gautam2016@gmail.com>
2021-08-16 13:13:46 -0400

    files imported

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Counter from './components/Counter';
-import Message from './components/Message';
+import { DestructuringClass } from './components/DestructuringClass';
+
+import {
+  DestructuringProps,
+  DestructuringProps1,
+} from './components/DestructuringFunction';
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      React Basics : Destructuring using Props and state
+      <hr />
+      <DestructuringProps name='Shikshya' profession='Programming' />
+      <DestructuringProps1 name='Nanu' profession='Coding' />
+      <DestructuringClass name='Snowie' Pet='Dog' />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
