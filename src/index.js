b6de318d7906811a4551b5043767f1a4d8972ef9
Shikshya Gautam <nanu.gautam2016@gmail.com>
2021-08-17 13:37:47 -0400

    files imported

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MethodsAsProps from './components/MethodsAsProps';
 import Props from './components/Props';
 import Props1 from './components/props1';
 
 const App = () => {
   return (
     <div>
-      <h1>Props in Functional Component</h1>
+      React Basics : Props in Functional Component
+      <hr />
       <Props name='Shikshya' profession='Coder'>
         This is a children prop.
         <button>click</button>
       </Props>
       <Props name='Nanu' profession='Programmer' />
-
       <h1>Props in Class Component</h1>
       <Props1 name='Shikshya' profession='Coder' />
       <Props1 name='Nanu' profession='Programmer' />
+      <MethodsAsProps />
     </div>
   );
 };
