d80a90e7da291688c90a27011065644de952699d
Shikshya Gautam <nanu.gautam2016@gmail.com>
2021-08-17 13:37:31 -0400

    child component for method

---
+++ src/components/ChildComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ChildComponent(props) {
+  return (
+    <div>
+      <button onClick={props.greetHandler}>Greet</button>
+    </div>
+  );
+}
+
+export default ChildComponent;
